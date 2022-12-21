32c9effb56b9a193e670f23aab599b264bec7583
Mason-001 <119235096+Mason-0001@users.noreply.github.com>
2022-12-20 22:31:45 -0500

    Updated file

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,7 +1,7 @@
 import pandas as pd
-
+#read csv
 data = pd.read_csv('PyBank/Resources/budget_data.csv')
-
+#calculate each individual variable
 total_months = len(data)
 net_total = data["Profit/Losses"].sum()
 ave_chg = round(data["Profit/Losses"].diff().mean(),2)
@@ -9,7 +9,7 @@ g_inc_m = data["Date"][data["Profit/Losses"].diff().argmax()]
 g_inc_p = round(data["Profit/Losses"].diff().max())
 g_dec_m = data["Date"][data["Profit/Losses"].diff().argmin()]
 g_dec_p = round(data["Profit/Losses"].diff().min())
-
+#print statement with variables included
 print(f"""
 Financial Analysis
 
